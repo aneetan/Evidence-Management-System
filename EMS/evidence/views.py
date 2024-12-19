01678cd795b8e616f111dac46287f5987411e1e8
aneetan <anitaneupane2061@gmail.com>
2024-12-19 19:46:40 +0545

    Initialized the contract using abi

--- EMS/evidence/views.py
+++ EMS/evidence/views.py
@@ -1,10 +1,14 @@
 import requests
+import os
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.conf import settings
 from django.contrib import messages
 from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def home(request):
@@ -17,7 +21,121 @@ def view(request):
     return render(request, 'view.html')
 
 def save_to_blockchain(ipfs_hash, case_id):
+    #Connect to blockchain
+    ganache_url = "http://127.0.0.1:7545"
+    w3 = Web3(Web3.HTTPProvider(ganache_url))
+
+    if not w3.is_connected():
+        raise Exception("Failed to connect to blockchain")
     
+    # 2. Contract ABI and Address
+    contract_address = Web3.to_checksum_address(os.getenv("CONTRACT_ADDRESS"))
+    contract_abi = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "hashID",
+				"type": "string"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "userAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timeStamp",
+				"type": "uint256"
+			}
+		],
+		"name": "HashStored",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_hashID",
+				"type": "string"
+			}
+		],
+		"name": "storeHash",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_hashID",
+				"type": "string"
+			}
+		],
+		"name": "getHashDetails",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "hashRecords",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "hashID",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "userAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timeStamp",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+    ]
+
+    # 3. Initialize Contract
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+    
+
+    
+
 
 
 @csrf_exempt
